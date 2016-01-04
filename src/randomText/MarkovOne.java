e7f042fce74e091bf7fb0bf596a24a2616fa5868
FS1360472174 <1015602538@qq.com>
2016-01-04 22:34:03 +0800

    fix bug of getFollows

--- src/randomText/MarkovOne.java
+++ src/randomText/MarkovOne.java
@@ -9,29 +9,32 @@ public class MarkovOne extends AbstractMarkov {
 
 	@Override
 	public String getRandomText(int numChars) {
-		if(text==null){
+		if (text == null) {
 			return "";
 		}
-		StringBuilder sb=new StringBuilder();
-		int index=0;
-		for(int i=0;i<numChars;i++){
-			index=random.nextInt(text.length());
+		StringBuilder sb = new StringBuilder();
+		int index = 0;
+		for (int i = 0; i < numChars; i++) {
+			index = random.nextInt(text.length());
 			sb.append(text.charAt(index));
 		}
 		return sb.toString();
 	}
+
 	/**
 	 * get the following character of key in text
+	 * 
 	 * @param key
 	 * @return
 	 */
-	public List<String>getFollows(String key){
-		ArrayList<String>followString=new ArrayList<String>();
-		String str=null;
-		for(int i=0;i<=text.length()-key.length();i++){
-			str=text.substring(i,i+key.length());
-			if(str.equals(key)&&i<text.length()-1){
-				followString.add(text.substring(i+1,i+2));
+	public List<String> getFollows(String key) {
+		ArrayList<String> followString = new ArrayList<String>();
+		String str = null;
+		int index = key.length();
+		for (int i = 0; i <= text.length() - index; i++) {
+			str = text.substring(i, i + index);
+			if (str.equals(key) && i < text.length() - index) {
+				followString.add(text.substring(i + index, i + index + 1));
 			}
 		}
 		return followString;
