f157b0e5413a94b9d820c29885d00a53c9ad36bf
FS1360472174 <1015602538@qq.com>
2016-01-13 14:45:52 +0800

    update

--- src/randomText/AbstractMarkov.java
+++ src/randomText/AbstractMarkov.java
@@ -1,5 +1,7 @@
 package randomText;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public abstract class AbstractMarkov implements IMarkov {
@@ -14,5 +16,23 @@ public abstract class AbstractMarkov implements IMarkov {
 	
 	public void setTraining(String s){
 		text = s.trim();
+	}
+	/**
+	 * get the following character of key in text
+	 * 
+	 * @param key
+	 * @return
+	 */
+	public List<String> getFollows(String key) {
+		ArrayList<String> followString = new ArrayList<String>();
+		String str = null;
+		int index = key.length();
+		for (int i = 0; i <= text.length() - index; i++) {
+			str = text.substring(i, i + index);
+			if (str.equals(key) && i < text.length() - index) {
+				followString.add(text.substring(i + index, i + index + 1));
+			}
+		}
+		return followString;
 	}
 }
